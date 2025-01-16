76f7f019f8d721a5e94bc53f569faa59018369f2
erdemdgl <92667120+erdemdgl@users.noreply.github.com>
2025-01-16 20:57:43 +0300

    Add files via upload

---
+++ Homework1/lcd.c
@@ -0,0 +1,116 @@
+#include "stdbool.h"
+#include "stdint.h"
+#include "inc/hw_ints.h"
+#include "inc/hw_types.h"
+#include "inc/hw_memmap.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+#include "Lcd.h"
+
+void Lcd_init() {
+
+        SysCtlPeripheralEnable(LCDPORTENABLE);
+        GPIOPinTypeGPIOOutput(LCDPORT, 0xFF);
+
+        SysCtlDelay(50000);
+
+        GPIOPinWrite(LCDPORT, RS,  0x00 );
+
+        //30 hex sending processes
+        GPIOPinWrite(LCDPORT, D4 | D5 | D6 | D7,  0x30 );
+        GPIOPinWrite(LCDPORT, E, 0x02);
+        SysCtlDelay(10);
+        GPIOPinWrite(LCDPORT, E, 0x00);
+
+        SysCtlDelay(50000);
+
+        GPIOPinWrite(LCDPORT, D4 | D5 | D6 | D7,  0x30 );
+        GPIOPinWrite(LCDPORT, E, 0x02);
+        SysCtlDelay(10);
+        GPIOPinWrite(LCDPORT, E, 0x00);
+
+        SysCtlDelay(50000);
+
+        GPIOPinWrite(LCDPORT, D4 | D5 | D6 | D7,  0x30 );
+        GPIOPinWrite(LCDPORT, E, 0x02);
+        SysCtlDelay(10);
+        GPIOPinWrite(LCDPORT, E, 0x00);
+
+        SysCtlDelay(50000);
+
+        GPIOPinWrite(LCDPORT, D4 | D5 | D6 | D7,  0x20 );
+        GPIOPinWrite(LCDPORT, E, 0x02);
+        SysCtlDelay(10);
+        GPIOPinWrite(LCDPORT, E, 0x00);
+        //finish 30 hex sending processes
+
+        SysCtlDelay(50000);
+
+        Lcd_command(0x28);    //4-bit mode, 2 line
+        SysCtlDelay(50000);
+        Lcd_command(0x0C);    // Screen on, cursor off
+        SysCtlDelay(50000);
+        Lcd_command(0x06);    // Writing direction to the right
+        SysCtlDelay(50000);
+        //Lcd_Komut(0x80);
+        //Lcd_Komut(0x0f);
+        Lcd_clear();
+
+}
+void Lcd_command(unsigned char c) {
+
+        GPIOPinWrite(LCDPORT, D4 | D5 | D6 | D7, (c & 0xf0) );
+        GPIOPinWrite(LCDPORT, RS, 0x00);
+        GPIOPinWrite(LCDPORT, E, 0x02);
+        SysCtlDelay(50000);
+        GPIOPinWrite(LCDPORT, E, 0x00);
+
+        SysCtlDelay(50000);
+
+        GPIOPinWrite(LCDPORT, D4 | D5 | D6 | D7, (c & 0x0f) << 4 );
+        GPIOPinWrite(LCDPORT, RS, 0x00);
+        GPIOPinWrite(LCDPORT, E, 0x02);
+        SysCtlDelay(10);
+        GPIOPinWrite(LCDPORT, E, 0x00);
+
+        SysCtlDelay(50000);
+
+}
+
+void Lcd_Putch(unsigned char d) {
+
+        GPIOPinWrite(LCDPORT, D4 | D5 | D6 | D7, (d & 0xf0) );
+        GPIOPinWrite(LCDPORT, RS, 0x01);
+        GPIOPinWrite(LCDPORT, E, 0x02);
+        SysCtlDelay(10);
+        GPIOPinWrite(LCDPORT, E, 0x00);
+
+        SysCtlDelay(50000);
+
+        GPIOPinWrite(LCDPORT, D4 | D5 | D6 | D7, (d & 0x0f) << 4 );
+        GPIOPinWrite(LCDPORT, RS, 0x01);
+        GPIOPinWrite(LCDPORT, E, 0x02);
+        SysCtlDelay(10);
+        GPIOPinWrite(LCDPORT, E, 0x00);
+
+        SysCtlDelay(50000);
+
+}
+void Lcd_Goto(char x, char y){
+        if(x==1)
+            Lcd_command(0x80+((y-1)%16));
+        else
+            Lcd_command(0xC0+((y-1)%16));
+}
+
+void Lcd_clear(void){
+    Lcd_command(0x01);
+        SysCtlDelay(10);
+}
+
+void Lcd_Puts( char* s){
+
+        while(*s)
+                Lcd_Putch(*s++);
+}
+
